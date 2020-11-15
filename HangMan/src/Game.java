8d3616911bc894ed5915f4139d20cd9665c70172
Plora Tran <ploratran@gmail.com>
2020-11-14 20:53:57 -0800

    display game instruction in Main + implement end game method

--- HangMan/src/Game.java
+++ HangMan/src/Game.java
@@ -4,6 +4,10 @@
 public class Game {
     // Fields:
     public String guessMovie;
+    public String rightChar;
+    public String wrongChar;
+    public int point;
+    public boolean hasWon;
 
     // Constructor:
     // initialize a MovieList object with file name:
@@ -11,7 +15,45 @@ public class Game {
         MovieList movieList = new MovieList(pathName);
         // define guess movie name from .getRandomMovie() in MovieList object:
         guessMovie = movieList.generateRandomMovie();
+        // System.out.println(guessMovie.replaceAll("\\s", "").length());
+        rightChar = "";
+        wrongChar = "";
+        point = 0;
+        hasWon = false;
     }
 
     // Methods:
+    /**
+     * Method to hide guessMovie with "_"
+     *
+     */
+    public String hiddenMovieTitle() {
+        return guessMovie.replaceAll("[a-zA-Z]", "_");
+    }
+
+    public int hiddenMovieTitleNoSpaces() {
+        return this.guessMovie.replaceAll("\\s", "").length();
+    }
+
+    /**
+     * Method to decide when the game ends
+     * check if "point" reaches to 10 => return true as game ends
+     * check if the hidden title is not all "_", means all chars revealed => return true
+     * else return false;
+     */
+    public boolean gameEnded() {
+        if (this.point >= 10) {
+            return true;
+        }
+
+        if (!hiddenMovieTitle().contains("_")) {
+            hasWon = true;
+            return true;
+        }
+
+        return false;
+    }
+
+
+
 }
--- HangMan/src/Main.java
+++ HangMan/src/Main.java
@@ -1,9 +1,15 @@
 import java.util.*;
-import java.io.*;
+import java.util.regex.Pattern.*;
 
 public class Main {
     public static void main (String[] args) {
         // initialize new game with movies in movieList.txt:
         Game game = new Game("movieList.txt");
+
+        System.out.println("Guess movie is: " + game.guessMovie);
+
+        System.out.println("Welcome to Hang Man Game. Let's get started!\n");
+        System.out.println("The movie title has with " + game.hiddenMovieTitleNoSpaces() + " characters.");
+        System.out.println(game.hiddenMovieTitle());
     }
 }
